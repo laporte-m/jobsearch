42355252cd3ec562ce4d14a9937a09945b698f82
Matt Laporte <Matt.L.Laporte@gmail.com>
2015-09-09 21:49:47 -0400

    Added function for retrieval of database entries, and separated database connection functions

--- jobsearch.py
+++ jobsearch.py
@@ -15,39 +15,57 @@ def add_app(conn, values):
                              Contact, Title, Type, Pay, Note) 
             VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)''', values)
 
-try:
-    conn = sqlite3.connect(db)
-    crs = conn.cursor()
-    crs.execute('''CREATE TABLE IF NOT EXISTS {}
-                   (Id INTEGER PRIMARY KEY, Date TEXT, 
-                    Deadline TEXT, Org TEXT, Source TEXT,
-                    Contact TEXT, Title TEXT, Type TEXT,
-                    Pay REAL, Note TEXT)'''.format(t))
-    crs.execute('PRAGMA table_info({})'.format(t))
-    names = [r[1:] for r in crs.fetchall()]
+def main():
+    try:
+        conn = sqlite3.connect(db)
+        crs = conn.cursor()
+        crs.execute('''CREATE TABLE IF NOT EXISTS {}
+                       (Id INTEGER PRIMARY KEY, Date TEXT, 
+                        Deadline TEXT, Org TEXT, Source TEXT,
+                        Contact TEXT, Title TEXT, Type TEXT,
+                        Pay REAL, Note TEXT)'''.format(t))
+        crs.execute('PRAGMA table_info({})'.format(t))
+        names = [r[1:] for r in crs.fetchall()]
+        
+        return conn, names
 
-except sqlite3.Error as e:
-    print("Error {}:".format(e.args[0]))
-    sys.exit(1)
+    except sqlite3.Error as e:
+        print("Error {}:".format(e.args[0]))
+        sys.exit(1)
 
-while True:
-    values = ()
-    print("Enter details for new application:")
-    print("(or type 'exit' to save last entry and exit)")
-    for i, name in enumerate(names):
-        if i: 
-            value = input('{}: '.format(name[0]))
-            values += (value,)
-            if value == 'exit': 
-                break
-    if 'exit' in values:
-        break
-    else:
-        add_app(conn, values)
+def retrieve(id=None):
+    ''' Return entry with given Id, else return all entries '''
+    conn, names = main()
+    with conn:
+        crs = conn.cursor()
+        if id:
+            try:
+                crs.execute('SELECT * FROM Apps WHERE Id={}'.format(id))
+            except:
+                print("Invalid entry Id!")
+        else:
+            crs.execute('SELECT * FROM Apps')
+        return crs.fetchall()    
 
-if conn: 
-    conn.close()
+if __name__ == '__main__':
+    conn, names = main()
+    while True:
+        values = ()
+        print("Enter details for new application:")
+        print("(or type 'exit' to save last entry and exit)")
+        for i, name in enumerate(names):
+            if i: 
+                value = input('{}: '.format(name[0]))
+                values += (value,)
+                if value == 'exit': 
+                    break
+        if 'exit' in values:
+            break
+        else:
+            add_app(conn, values)
 
+    if conn: 
+        conn.close()
 
         
     
